4c3f1268e6a7f8d194d7a7093f08fbd92024035e
Alex Nguyen <alexnguyen.dev@tutanota.com>
2021-09-13 12:31:45 +1000

    chapter 3 & chapter 4

--- Exercises/Chapter03/Exercises.cs
+++ Exercises/Chapter03/Exercises.cs
@@ -1,7 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Boc.Commands;
 //using System.Configuration;
 using LaYumba.Functional;
+using Shouldly;
+using Xunit;
+using static LaYumba.Functional.F;
+using Enum = System.Enum;
 
 namespace Exercises.Chapter3
 {
@@ -12,6 +20,34 @@ namespace Exercises.Chapter3
 
       // Enum.Parse<DayOfWeek>("Friday") // => Some(DayOfWeek.Friday)
       // Enum.Parse<DayOfWeek>("Freeday") // => None
+      public static Option<DayOfWeek> Parse(string s) =>
+         Enum.TryParse(s, out DayOfWeek result)
+            ? Some(result)
+            : None;
+
+      [Fact]
+      public static void WhenParseCalledOnDayOfWeek_GetOptionWithSome()
+      {
+         // arrange
+         var day = "Friday";
+         // act
+         var result = Parse(day);
+         // assert
+         result.ShouldBe(Some(DayOfWeek.Friday));
+         result.ShouldBe(DayOfWeek.Friday);
+         result.ShouldBeOfType<Option<DayOfWeek>>();
+      }
+
+      [Fact]
+      public static void WhenParseCalledWithInvalidDay_ReturnsNone()
+      {
+         // arrange
+         var day = "Freeday";
+         // act
+         var result = Parse(day);
+         // assert
+         result.ShouldBe(None);
+      }
 
       // 2 Write a Lookup function that will take an IEnumerable and a predicate, and
       // return the first element in the IEnumerable that matches the predicate, or None
@@ -20,17 +56,93 @@ namespace Exercises.Chapter3
       // bool isOdd(int i) => i % 2 == 1;
       // new List<int>().Lookup(isOdd) // => None
       // new List<int> { 1 }.Lookup(isOdd) // => Some(1)
+      // List<T> --> (T -> bool) --> Option<T>
+      // using linq
+      // public static Option<T> Lookup<T>(this List<T> list, Func<T, bool> predicate) =>
+      //    list.Where(predicate).Any()
+      //       ? Some(list.Where(predicate).FirstOrDefault())
+      //       : None;
+
+      public static Option<T> Lookup<T>(this List<T> list, Func<T, bool> predicate)
+      {
+         foreach (var item in list)
+         {
+            if (predicate(item)) return Some(item);
+         }
+
+         return None;
+      }
+
+      [Fact]
+      public static void Lookup_WithNoMatch_ReturnNone()
+      {
+         // arrange
+         var list = new List<int> { 5, 10, 15 };
+         // act
+         var result = list.Lookup((item) => item % 12 == 0);
+
+         // assert
+         result.ShouldBe(None);
+         result.ShouldBeOfType<Option<int>>();
+      }
+
+      [Fact]
+      public static void Lookup_WithMatch_ReturnFirstSome()
+      {
+         // arrange
+         var list = new List<int> { 5, 10, 15 };
+         // act
+         var result = list.Lookup((item) => item % 5 == 0);
+
+         // assert
+         result.ShouldBe(5);
+         result.ShouldBe(Some(5));
+         result.ShouldBeOfType<Option<int>>();
+      }
+
 
       // 3 Write a type Email that wraps an underlying string, enforcing that it’s in a valid
       // format. Ensure that you include the following:
       // - A smart constructor
       // - Implicit conversion to string, so that it can easily be used with the typical API
       // for sending emails
-      
+
+      public struct Email
+      {
+         private string Value { get;  }
+         static readonly Regex Regex = new(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+         private Email(string value) => Value = value;
+            // if (!IsValid(value))
+            // {
+            //    throw new ArgumentException($"{value} is not a valid email");
+            // }
+
+         public static Option<Email> Create(string value) => Regex.IsMatch(value) ? Some(new Email(value)) : None;
+
+         // private static bool IsValid(string value) => value.Contains("@") && value.Length < 20;
+
+         public static implicit operator string(Email e) => e.Value;
+      }
+
+      [Fact]
+      public static void WhenCreated_ConvertsToString()
+      {
+         // arrange
+         var expectedString = "abc@def.com";
+         // act
+         var result = Email.Create(expectedString);
+         var email = result.Match(() => "email was empty.. womp womp", (e) => e);
+         // assert
+         email.ShouldBe(expectedString);
+      }
+
       // 4 Take a look at the extension methods defined on IEnumerable inSystem.LINQ.Enumerable.
       // Which ones could potentially return nothing, or throw some
       // kind of not-found exception, and would therefore be good candidates for
       // returning an Option<T> instead?
+      // TODO: check ienums
+
    }
 
    // 5.  Write implementations for the methods in the `AppConfig` class
@@ -51,12 +163,14 @@ namespace Exercises.Chapter3
 
       public Option<T> Get<T>(string name)
       {
-         throw new NotImplementedException("your implementation here...");
+         return source[name] == null ? None : Some((T)Convert.ChangeType(source[name], typeof(T)));
       }
 
       public T Get<T>(string name, T defaultValue)
       {
-         throw new NotImplementedException("your implementation here...");
+         return Get<T>(name).Match((
+            () => defaultValue),
+            (val) => val);
       }
    }
 }
--- Exercises/Chapter04/Exercises.cs
+++ Exercises/Chapter04/Exercises.cs
@@ -1,50 +1,209 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FizzWare.NBuilder;
+using FizzWare.NBuilder.Generators;
 using LaYumba.Functional;
+using LaYumba.Functional.Option;
+using Shouldly;
+using Xunit;
 
 namespace Exercises.Chapter4
 {
-   static class Exercises
-   {
-      // 1 Implement Map for ISet<T> and IDictionary<K, T>. (Tip: start by writing down
-      // the signature in arrow notation.)
+    using static F;
 
-      // 2 Implement Map for Option and IEnumerable in terms of Bind and Return.
+    public static class Exercises
+    {
+        // 1 Implement Map for ISet<T> and IDictionary<K, T>. (Tip: start by writing down
+        // the signature in arrow notation.)
+        public static ISet<R> Map<T, R>(this ISet<T> set, Func<T, R> f)
+        {
+            var newSet = new HashSet<R>();
+            foreach (var item in set)
+            {
+                newSet.Add(f(item));
+            }
 
-      // 3 Use Bind and an Option-returning Lookup function (such as the one we defined
-      // in chapter 3) to implement GetWorkPermit, shown below. 
+            return newSet;
+        }
 
-      // Then enrich the implementation so that `GetWorkPermit`
-      // returns `None` if the work permit has expired.
+        [Fact]
+        public static void Map_AppliesInParseGivenFunc()
+        {
+            // arrange
+            var f = new Func<string, int>((s) => Int32.Parse(s));
+            var testSet = new HashSet<string>(new List<string>(new[] { "5", "10", "5" }));
+            var expectedSet = new HashSet<int>(new List<int>(new[] { 5, 10 }));
 
-      static Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> people, string employeeId)
-      {
-         throw new NotImplementedException();
-      }
+            // act
+            var result = testSet.Map(f);
 
-      // 4 Use Bind to implement AverageYearsWorkedAtTheCompany, shown below (only
-      // employees who have left should be included).
+            // assert
+            result.ShouldBe(expectedSet);
+        }
 
-      static double AverageYearsWorkedAtTheCompany(List<Employee> employees)
-      {
-         // your implementation here...
-         throw new NotImplementedException();
-      }
-   }
+        public static IDictionary<K, R> Map<K, T, R>(this IDictionary<K, T> dict, Func<T, R> f)
+        {
+            var newDict = new Dictionary<K, R>();
+            foreach (var key in dict.Keys)
+            {
+                newDict[key] = f(dict[key]);
+            }
 
-   public struct WorkPermit
-   {
-      public string Number { get; set; }
-      public DateTime Expiry { get; set; }
-   }
+            return newDict;
+        }
 
-   public class Employee
-   {
-      public string Id { get; set; }
-      public Option<WorkPermit> WorkPermit { get; set; }
+        [Fact]
+        public static void Map_AppliesToValuesInParseGivenFunc()
+        {
+            // arrange
+            var f = new Func<string, int>((s) => Int32.Parse(s));
+            var testDict = new Dictionary<string, string>
+            {
+                { "one", "10" },
+                { "asdfjsdkf", "100000" },
+                { "values", "105" },
+                { "again", "-1" },
+            };
+            var expectedDict = new Dictionary<string, int>
+            {
+                { "one", 10 },
+                { "asdfjsdkf", 100000 },
+                { "values", 105 },
+                { "again", -1 },
+            };
 
-      public DateTime JoinedOn { get; }
-      public Option<DateTime> LeftOn { get; }
-   }
+            // act
+            var result = testDict.Map(f);
+
+            // assert
+            result.ShouldBe(expectedDict);
+        }
+
+
+        // 2 Implement Map for Option and IEnumerable in terms of Bind and Return.
+        // use bind/ return
+        public static Option<R> Map<T, R>(this Option<T> option, Func<T, R> f) =>
+            option.Bind(innerVal => Some(f(innerVal)));
+
+        public static IEnumerable<R> Map<T, R>(this IEnumerable<T> enumerable, Func<T, R> f) =>
+            enumerable.Bind(t => List(f(t)));
+        // TODO: test both
+
+        // 3 Use Bind and an Option-returning Lookup function (such as the one we defined
+        // in chapter 3) to implement GetWorkPermit, shown below.
+
+        public static Option<R> Lookup<K, R>(this IDictionary<K, R> dict, K key) =>
+            dict.TryGetValue(key, out R val) ? Some(val) : F.None;
+
+        // Then enrich the implementation so that `GetWorkPermit`
+        // returns `None` if the work permit has expired.
+
+        static Option<WorkPermit> GetWorkPermit(Dictionary<string, Employee> people, string employeeId) =>
+            people.Lookup(employeeId).Bind(employee => employee.WorkPermit);
+
+        [Fact]
+        public static void GetWorkPermit_GetsCorrectValue()
+        {
+            // arrange
+            var empId = GetRandom.String(10);
+            var workPermit = Builder<WorkPermit>.CreateNew().Build();
+            var emp = Builder<Employee>
+                .CreateNew()
+                .With(m => m.WorkPermit, workPermit)
+                .Build();
+            var people = new Dictionary<string, Employee> { { empId, emp } };
+            // act
+            var result = GetWorkPermit(people, empId);
+            // assert
+            result.ShouldBe(Some(workPermit));
+            result.ShouldBe(workPermit);
+            result.ShouldBeOfType<Option<WorkPermit>>();
+        }
+
+        [Fact]
+        public static void GetWorkPermit_EmployeeNotInPeople_GetsCorrectValue()
+        {
+            // arrange
+            var empId = GetRandom.String(10);
+            var workPermit = Builder<WorkPermit>.CreateNew().Build();
+            var emp = Builder<Employee>
+                .CreateNew()
+                .With(m => m.WorkPermit, workPermit)
+                .Build();
+            var people = new Dictionary<string, Employee> { };
+            // act
+            var result = GetWorkPermit(people, empId);
+            // assert
+            result.ShouldBe(None);
+            result.ShouldBeOfType<Option<WorkPermit>>();
+        }
+
+        // 4 Use Bind to implement AverageYearsWorkedAtTheCompany, shown below (only
+        // employees who have left should be included).
+
+        static double AverageYearsWorkedAtTheCompany(List<Employee> employees) =>
+            employees
+                .Bind(e => e.LeftOn.Map(leftOn => YearsBetween(leftOn, e.JoinedOn)))
+                .Average();
+
+        private static int YearsBetween(DateTime leftOn, DateTime joinedOn) => (leftOn - joinedOn).Days / 365;
+
+        [Fact]
+        public static void AverageYearsWorkedAtCompany_WithEmployeesWhoHaveAllLeft_GivesExpectedValue()
+        {
+            // arrange
+            var startDateTime = GetRandom.DateTime();
+            var duration = 30;
+            var expectedEmployees = Builder<Employee>
+                .CreateListOfSize(10)
+                .All()
+                .With(m => m.JoinedOn, startDateTime)
+                .With(m => m.LeftOn, Some(startDateTime.AddYears(duration)))
+                .Build()
+                .ToList();
+
+            // act
+            var result = AverageYearsWorkedAtTheCompany(expectedEmployees);
+
+            // assert
+            result.ShouldBe(duration);
+        }
+
+        [Fact]
+        public static void AverageYearsWorkedAtCompany_WithNoEmployeesThatHaveLeft_Throws()
+        {
+            // only supposed to count left employees
+            // arrange
+            var startDateTime = GetRandom.DateTime();
+            var expectedEmployees = Builder<Employee>
+                .CreateListOfSize(10)
+                .All()
+                .With(m => m.JoinedOn, startDateTime)
+                .With(m => m.LeftOn, None)
+                .Build()
+                .ToList();
+
+            // act
+            var exception = Should.Throw<InvalidOperationException>(() => AverageYearsWorkedAtTheCompany(expectedEmployees));
+
+            // assert
+            exception.Message.ShouldBe("Sequence contains no elements");
+        }
+    }
+
+    public struct WorkPermit
+    {
+        public string Number { get; set; }
+        public DateTime Expiry { get; set; }
+    }
+
+    public class Employee
+    {
+        public string Id { get; set; }
+        public Option<WorkPermit> WorkPermit { get; set; }
+
+        public DateTime JoinedOn { get; set; }
+        public Option<DateTime> LeftOn { get; set; }
+    }
 }
