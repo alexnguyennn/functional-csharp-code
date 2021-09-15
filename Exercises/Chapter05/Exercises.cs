d25d1ff5cca3a57cf7a61a4ba941ebfd36948959
Alex Nguyen <alexnguyen.dev@tutanota.com>
2021-09-15 21:32:43 +1000

    chapter 5

--- Exercises/Chapter05/Exercises.cs
+++ Exercises/Chapter05/Exercises.cs
@@ -14,15 +14,18 @@ namespace Exercises.Chapter5
       // `OrderByDescending`, `Take` and `Average`, which we used to implement `AverageEarningsOfRichestQuartile`:
       static decimal AverageEarningsOfRichestQuartile(List<Person> population)
          => population
-            .OrderByDescending(p => p.Earnings)
-            .Take(population.Count/4)
-            .Select(p => p.Earnings)
-            .Average();
+            .OrderByDescending(p => p.Earnings) // IEnumerable<T> -> (T -> int) -> IEnumerable<T>
+            .Take(population.Count / 4) // IEnumerable<T> -> int -> IEnumerable<T>
+            .Select(p => p.Earnings) // IEnumerable<T> -> (T -> int) -> IEnumerable<int>
+            .Average(); // IEnumerable<int> -> int
 
       // 2 Check your answer with the MSDN documentation: https://docs.microsoft.com/
       // en-us/dotnet/api/system.linq.enumerable. How is Average different?
+      // average doesn't yield so realises the whole chain - actually evaluates and redices to single value
 
       // 3 Implement a general purpose Compose function that takes two unary functions
       // and returns the composition of the two.
+      // (T -> X ) -> (X -> R) -> R
+      public static Func<T, R> Compose<T, X, R>(Func<T, X> f, Func<X, R> g) => x => g(f(x));
    }
 }
